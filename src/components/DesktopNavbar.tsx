ffa1ee50b93ca1105abd662b3c2fad1c9b509f45
Ayush Jain <ayushjain0761@gmail.com>
2023-06-12 08:58:26 -0700

    updated DesktopNavbar

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -9,7 +9,7 @@ type DesktopNavbarProps = {
 const DesktopNavbar = ({ navItems }: DesktopNavbarProps) => {
   return (
     <div className='fixed bottom-10 z-40 mx-auto hidden w-full items-center justify-center sm:flex'>
-      <div className='max-w-2xl rounded-full bg-black px-3 py-5 text-center text-white'>
+      <div className='max-w-2xl rounded-full bg-black px-2 py-2 text-center text-white'>
         <ul className='flex items-center justify-around gap-10 text-xl'>
           <li key={'arrow-up'} className='rounded-full bg-gray-700 p-3'>
             <SlArrowUp className='mx-auto cursor-pointer text-2xl' />
@@ -26,7 +26,7 @@ const DesktopNavbar = ({ navItems }: DesktopNavbarProps) => {
             key={'login'}
             className='rounded-full bg-gray-700 p-3 tracking-tight'
           >
-            <SlArrowUp className='mx-auto cursor-pointer text-2xl' />
+            <Link href={'/login'}>Log in</Link>
           </li>
         </ul>
       </div>
